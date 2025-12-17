02c4d5081ad2c455347e59f7fc63a5f82642d545
Vassilis Papastergios <papster@csd.auth.gr>
2025-12-18 00:25:16 +0200

    [quick-and-dirty] Create proof-of-concept script for synthcity generators

---
+++ generators/quick_and_dirty_synthcity_test.py
@@ -0,0 +1,86 @@
+# Make sure you have uv: https://docs.astral.sh/uv/guides/install-python/
+# only for the first time: from the root directory of the project: `uv venv && uv pip install -r requirements.txt && cd generators && uv run quick_and_dirty_synthcity_test.py`
+# for any next execution: from the `generators` directory: `uv run quick_and_dirty_synthcity_test.py`
+
+# The script takes approximately 3mins in cobra
+
+from enum import Enum
+from sklearn.datasets import load_diabetes
+from synthcity.plugins import Plugins
+from synthcity.plugins.core.dataloader import GenericDataLoader
+import pandas as pd
+
+
+N_SAMPLES = 1000
+
+# DUPLICATED FOR CONVENIENCE. This should not be here on actual implementation
+class SynthcityModelOption(Enum):
+    TVAE = 'tvae'
+    CTGAN = 'ctgan'
+    NFLOW = 'nflow'
+    DDPM = 'ddpm'
+    ARF = 'arf'
+    MARGINAL_DISTRIBUTIONS = 'marginal_distributions'
+
+# DUPLICATED FOR CONVENIENCE. This should not be here on actual implementation
+class SynthcitySettings:
+    def __init__(
+        self, 
+        model_name: SynthcityModelOption,
+        n_samples: int = 1000,
+    ):
+        self.model_name = model_name.value
+        self.n_samples = n_samples
+
+    def to_dict(self) -> dict:
+        return vars(self)
+
+    @classmethod
+    def from_dict(cls, settings_dict: dict):
+        return cls(**settings_dict)
+    
+
+class SynthcityGenerator():
+    """Hierarchy class for generators from the syntcity package
+    https://github.com/vanderschaarlab/synthcity/blob/main/tutorials/tutorial0_basic_examples.ipynb
+    """
+    
+    def __init__(self, settings: SynthcitySettings):
+        super().__init__()
+        self.settings = settings.to_dict()
+        self.generator = None
+    
+    def generate(self, X_initial, y_initial, task):
+        loader = GenericDataLoader(
+            pd.concat([X_initial, y_initial], axis=1),
+            target_column=y_initial.columns[0]
+        )
+        self.generator = Plugins().get(self.settings["model_name"])
+        self.generator.fit(loader)
+        return self.generator.generate(count=self.settings["n_samples"]).dataframe()
+
+def main():
+    from time import time
+    
+    start = time()
+    X, y = load_diabetes(return_X_y=True, as_frame=True)
+
+    for model in SynthcityModelOption:
+        model_start = time()
+        print("=" * 50)
+        print(model.name)
+        print("=" * 50)
+        
+        settings = SynthcitySettings(model_name=model, n_samples=N_SAMPLES)
+        generator = SynthcityGenerator(settings=settings)
+        generated_data = generator.generate(X, y.to_frame(), "")
+        
+        print(generated_data.describe())
+        print(f"Time elapsed: {time() - model_start} sec")
+    
+    print()
+    print()
+    print(f"Total elapsed time: {time() - start} sec")
+    
+if __name__ == '__main__':
+    main()
